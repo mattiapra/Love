c557a1d5e158f3aaaaf3eb6ab45f5551d5a61aa4
= <pramattia@gmail.com>
2022-11-25 17:52:04 +0100

    rewrite

---
+++ src/layouts/Compleanno2022.vue
@@ -0,0 +1,16 @@
+<script setup>
+const ready = ref(true)
+
+useHead({
+  title: 'Buon 21esimo compleanno amore mio',
+})
+</script>
+
+<template>
+  <router-view v-slot="{ Component }">
+    <transition name="opacity" mode="out-in">
+      <Boot v-if="!ready" :load-from="2001" :load-to="2022" @ready="ready = true" />
+      <component :is="Component" v-else />
+    </transition>
+  </router-view>
+</template>
